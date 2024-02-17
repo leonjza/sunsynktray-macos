cd1cf9cdeb72b5992d91354085cca2987875043f
Leon Jacobs <leonja511@gmail.com>
2024-02-17 16:24:20 +0200

    (fix) actually run the login helper delegate
    also fix a race condition with a semaphore when opening the target
    app

--- SunSynkTrayHelper/SunSynkTrayHelperApp.swift
+++ SunSynkTrayHelper/SunSynkTrayHelperApp.swift
@@ -1,8 +1,21 @@
+import SwiftUI
+import AppKit
+
 import Cocoa
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
+struct SunsynkTrayAppHelper: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
+    var body: some Scene {
+        Settings {
+
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let mainAppBundleIdentifier = "com.leonjza.sunsynktray"
         
@@ -17,11 +30,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
             
             let configuration = NSWorkspace.OpenConfiguration()
-            NSWorkspace.shared.openApplication(at: mainAppURL, configuration: configuration) { app, error in
+            let semaphore = DispatchSemaphore(value: 0) // rip 2 hours
+            
+            NSWorkspace.shared.openApplication(at: mainAppURL, configuration: configuration) { (app, error) in
                 if let error = error {
-                    print("Failed to open main app: \(error.localizedDescription)")
+                    print("Failed to open app: \(error.localizedDescription)")
                 }
+                semaphore.signal()
             }
+            semaphore.wait()
         }
         
         NSApp.terminate(nil)
